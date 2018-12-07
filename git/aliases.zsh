288ad6299e4a43686fc11cb9af72a02ce417a6b0
dgehrett <dgehrett@placester.com>
2018-12-06 21:16:02 -0600

    stuff

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -18,3 +18,5 @@ alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{\$1=\$2=\"\"; print \$0}' | \
            perl -pe 's/^[ \t]*//' | sed 's/ /\\\\ /g' | xargs git rm"
+alias gprune="git remote prune origin"
+alias gdel="git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -d"
